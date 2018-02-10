85a62a974a1748798bcccbb3ca0164b94e17a9fd
Jayprakash12345 <0freerunning@gmail.com>
2018-02-10 13:55:42 +0000

    Convert Checkpoint to use extension registration
    Bug: T186961
    Change-Id: If6587e06f2ea63ea1d04f6dde095ce5ad217ec20

--- Checkpoint.php
+++ Checkpoint.php
@@ -5,68 +5,16 @@
  * extension is good for testing purposes, etc. before implementation
  */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo "This is an extension to the MediaWiki software and cannot be used standalone";
-	die( 1 );
-}
-
-$wgExtensionCredits['other'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'Checkpoint',
-	'descriptionmsg' => 'checkpoint-desc',
-	'author'         => 'Ryan Schmidt',
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:Checkpoint',
-	'version'        => '0.2.0',
-);
-
-$wgMessagesDirs['Checkpoint'] = __DIR__ . '/i18n';
-
-$wgHooks['EditPageBeforeEditButtons'][] = 'efCheckpointButton';
-$wgHooks['PageContentSave'][] = 'efCheckpointSave';
-$wgHooks['GetFullURL'][] = 'efCheckpointReturn';
-
-function efCheckpointButton( &$editpage, &$buttons ) {
-	$attr = array(
-		'id'    => 'wpCheckpoint',
-		'name'  => 'wpCheckpoint',
-		'type'  => 'submit',
-		'value' => wfMessage( 'checkpoint' )->text(),
-		'title' => wfMessage( 'checkpoint-tooltip' )->text(),
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Checkpoint' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Checkpoint'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the Checkpoint extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
-	$buttons['checkpoint'] = Xml::element( 'input', $attr, '' );
-	return true;
-}
-
-function efCheckpointSave( WikiPage &$wikiPage, &$user, &$content, &$summary,
-	$isMinor, $isWatch, $section, &$flags, &$status ) {
-	global $wgRequest;
-
-	if ( $wgRequest->getCheck( 'wpCheckpoint' ) ) {
-		if ( $summary == '' ) {
-			// blank summary, so let's get an automatic one if
-			// applicable (the appending bit prevents autosummaries
-			// from appearing otherwise).
-			$old_content = $wikiPage->getContent( Revision::RAW ); // current revision
-			$summary = $wikiPage->getContentHandler()->getAutosummary( $old_content, $content, $flags );
-		}
-		$summary .= wfMessage( 'word-separator' )->text() . wfMessage( 'checkpoint-notice' )->text();
-	}
-	return true;
-}
-
-function efCheckpointReturn( $title, &$url, $query ) {
-	global $wgRequest;
-
-	if ( $wgRequest->wasPosted() && $wgRequest->getCheck( 'wpCheckpoint' ) ) {
-		$frag = $title->getFragmentForURL();
-		$querystr = strpos( $url, '?' ) ? '&' : '?'; // see if we need to append a ? or a &
-		if ( $frag == '' ) {
-			// just append our query to the end
-			$url .= $querystr . 'action=edit&preview=yes';
-		} else {
-			// do a string replace
-			$url = str_replace( $frag, $querystr . 'action=edit&preview=yes' . $frag, $url );
-		}
-	}
-	return true;
+	return;
+} else {
+	die( 'This version of the Checkpoint extension requires MediaWiki 1.25+' );
 }
